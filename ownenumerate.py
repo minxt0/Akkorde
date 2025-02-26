574575a9ce26255e8f5c99812135f067ea726dc4
minxto <tului.gueven@gmail.com>
2025-02-26 12:06:58 +0100

    added own Enumerate func

---
+++ ownenumerate.py
@@ -0,0 +1,10 @@
+def own_Enumerate(list:list,start:int=0):
+    locallist = list
+    iteration = start
+    enumerated_list =[]
+    for i in range(start):
+        locallist.pop(0)
+    for i in locallist:
+        enumerated_list.append((iteration,i))
+        iteration+=1
+    return(enumerated_list)
