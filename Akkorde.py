23c95097ce1ddb7ecd492fcd782a42b3e7332b05
minxto <tului.gueven@gmail.com>
2025-02-26 13:10:23 +0100

    changed tonleiter var to gundton

--- Akkorde.py
+++ Akkorde.py
@@ -1,17 +1,17 @@
-tonleiter = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+grundton = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 
 note = ""
 gr_terz = 4
 quinte = 7
-list_len = len(tonleiter)
+list_len = len(grundton)
 
 note = input("Note hier eingeben: ").upper()
 
-if note not in tonleiter:
+if note not in grundton:
     print("Ungültige Eingabe")
 else:
     #searching for position of note in list
-    for i,j in enumerate(tonleiter):
+    for i,j in enumerate(grundton):
         if j == note:
             notenpos = i 
             break    
@@ -25,5 +25,4 @@ else:
     elif quinte_pos >= list_len:
         quinte_pos = quinte_pos-list_len
     #output of Akkord
-    print(tonleiter[notenpos],tonleiter[gr_terz_pos],tonleiter[quinte_pos])
-
+    print(grundton[notenpos],grundton[gr_terz_pos],grundton[quinte_pos])
